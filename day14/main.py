779bb1e95d2a57fb2aacd6c4fdd1994244e82920
Floozutter <floozutter@gmail.com>
2019-12-19 23:35:13 -0800

    Add complete Day 14 solution!

--- day14/main.py
+++ day14/main.py
@@ -1,119 +1,85 @@
 from collections import Counter
+from math import ceil
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 
 with open(INPUTPATH) as ifile:
-    lines = ifile.read()
+    data = ifile.read()
 
-reqs = dict()
-for line in lines.strip().split("\n"):
-    recipe, thing = line.split("=>")
-    thingcount, thing = thing.strip().split(" ")
-    thingcount = int(thingcount)
-    comps = set()
-    for part in recipe.strip().split(", "):
-        compcount, compthing = part.strip().split(" ")
-        comps.add( (compthing, int(compcount.strip())) )
-    reqs[thing] = (thingcount, comps)
+def parse_reactions(inputstring):
+    reactionsdict = dict()
+    for line in inputstring.strip().split("\n"):
+        reactants, product = line.split("=>")
+        pcount, pname = product.strip().split(" ")
+        pcount = int(pcount)
+        reactantlist = list()
+        for reactant in reactants.strip().split(", "):
+            rcount, rname = reactant.strip().split(" ")
+            rcount = int(rcount)
+            reactantlist.append( (rcount, rname) )
+        reactionsdict[pname] = (pcount, tuple(reactantlist))
+    return reactionsdict
 
-def orecount(reqdict, thing):
-    reqlist = [thing]
-    extras = []
-    def oresonly(reqlist):
-        for req in reqlist:
-            if req != "ORE":
-                return False
-        return True
-    while not oresonly(reqlist):
-        newreqs = []
-        for req in reqlist:
-            if req == "ORE":
-                newreqs.append(req)
-                continue
-            elif req in extras:
-                extras.remove(req)
-                continue
-            outputcount, reqs4req = reqdict[req]
-            for i in range(outputcount - 1):
-                extras.append(req)
-            for reqreq, reqcount in reqs4req:
-                for i in range(reqcount):
-                    if reqreq in extras:
-                        extras.remove(reqreq)
-                    else:
-                        newreqs.append(reqreq)
-        reqlist = newreqs[:]
-    return len(reqlist)
-
-#print(orecount(reqs, "FUEL"))
-
-def orecount(reqdict, thing, amount):
-    reqcounter = Counter({thing: amount})
+def ore_requirement(reactionsdict, product, quantity):
+    reqdict = Counter({product: quantity})
     extras = Counter()
-    def oresonly(reqcounter):
-        for k, v in reqcounter.items():
-            if k != "ORE" and v > 0:
-                return False
-        return True
-
-    while not oresonly(reqcounter):
-        newreqcounter = Counter()
-        for k, v in reqcounter.items():
-            if k == "ORE":
-                newreqcounter["ORE"] += v
+    while not all(k == "ORE" or v == 0 for k, v in reqdict.items()):
+        # break when requirements are only ore
+        for k in list(reqdict.keys()):
+            if k == "ORE" or reqdict[k] == 0:
                 continue
-            for i in range(v):
-                if extras[k] > 0:
-                    extras[k] -= 1
-                    continue
-                outputcount, reqs4req = reqdict[k]
-                extras[k] += outputcount-1
-                for reqreq, reqcount in reqs4req:
-                    for j in range(reqcount):
-                        if extras[reqreq] > 0:
-                            extras[reqreq] -= 1
-                        else:
-                            newreqcounter[reqreq] += 1
-        reqcounter = Counter(newreqcounter)
-    return reqcounter["ORE"]
+            if extras[k] >= reqdict[k]:
+                extras[k] -= reqdict[k]
+                reqdict[k] = 0
+                continue
+            else:
+                reqdict[k] -= extras[k]
+                extras[k] = 0
+            products_per_reaction, reactant_reqs = reactionsdict[k]
+            reactions = ceil(reqdict[k] / products_per_reaction)
+            products = reactions * products_per_reaction
+            extras[k] += products - reqdict[k]
+            reqdict[k] = 0
+            for rr in reactant_reqs:
+                rcount, rname = rr
+                reqdict[rname] += rcount * reactions
+    return reqdict["ORE"]
 
-print(orecount(reqs, "FUEL", 1))
+reactions = parse_reactions(data)
+print(ore_requirement(reactions, "FUEL", 1))
 
-def addfueluntilnotenoughore(reqdict, oreamount):
-    fuel = 1
-    reqcounter = Counter({"FUEL": 1})
+
+def addfueluntilnomoreore(reactionsdict, available_ore, log=False):
+    fuel = 0
+    reqdict = Counter()
     extras = Counter()
-    def oresonly(reqcounter):
-        for k, v in reqcounter.items():
-            if k != "ORE" and v > 0:
-                return False
-        return True
-
     while True:
-        if oresonly(reqcounter):
-            print(fuel, reqcounter["ORE"])
-            if reqcounter["ORE"] > oreamount:
-                return fuel
+        if all(k == "ORE" or v == 0 for k, v in reqdict.items()):
+            if log and (fuel % 1000 == 0):
+                print("FUEL:", fuel, "| ORE:", f'{reqdict["ORE"]:,}')
+            if reqdict["ORE"] > available_ore:
+                return fuel - 1
             fuel += 1
-            reqcounter["FUEL"] += 1
-        newreqcounter = Counter()
-        for k, v in reqcounter.items():
-            if k == "ORE":
-                newreqcounter["ORE"] += v
+            reqdict["FUEL"] += 1
+        for k in list(reqdict.keys()):
+            if k == "ORE" or reqdict[k] == 0:
                 continue
-            for i in range(v):
-                if extras[k] > 0:
-                    extras[k] -= 1
-                    continue
-                outputcount, reqs4req = reqdict[k]
-                extras[k] += outputcount-1
-                for reqreq, reqcount in reqs4req:
-                    for j in range(reqcount):
-                        if extras[reqreq] > 0:
-                            extras[reqreq] -= 1
-                        else:
-                            newreqcounter[reqreq] += 1
-        reqcounter = Counter(newreqcounter)
+            if extras[k] >= reqdict[k]:
+                extras[k] -= reqdict[k]
+                reqdict[k] = 0
+                continue
+            else:
+                reqdict[k] -= extras[k]
+                extras[k] = 0
+            products_per_reaction, reactant_reqs = reactionsdict[k]
+            reactions = ceil(reqdict[k] / products_per_reaction)
+            products = reactions * products_per_reaction
+            extras[k] += products - reqdict[k]
+            reqdict[k] = 0
+            for rr in reactant_reqs:
+                rcount, rname = rr
+                reqdict[rname] += rcount * reactions
+    print("How did you get here?")
 
-print(addfueluntilnotenoughore(reqs, 1000000000000))
+print(addfueluntilnomoreore(reactions, 1000000000000, True))
