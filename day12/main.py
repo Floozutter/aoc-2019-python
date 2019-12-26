e14201873a1954680333f36031dec3e638acce83
Floozutter <floozutter@gmail.com>
2019-12-25 21:58:58 -0800

    Change Day 12 Part 1 solution

--- day12/main.py
+++ day12/main.py
@@ -1,89 +1,44 @@
+from functools import reduce
+from itertools import tee
 import re
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 
 with open(INPUTPATH) as ifile:
-    lines = ifile.read()
+    lines = ifile.read().strip().split("\n")
 
-data = lines.strip().split("\n")
-data = [list(map(int, re.findall(r"-?\d+\.?\d*", s))) for s in data]
+def parse_moonstring(line):
+    numstrs = re.findall(r"-?\d+\.?\d*", line)
+    return (tuple(map(int, numstrs)), (0, 0, 0))
 
-class Moon:
-    def __init__(self, postuple):
-        self.x, self.y, self.z = postuple
-        self.vx, self.vy, self.vz = (0, 0, 0)
+def add_tuples(a, b):
+    return tuple(map(sum, zip(a, b)))
 
-def printmoons(moonlist):
-    for moon in moons:
-        print("(", moon.x, moon.y, moon.z, ")",
-              "(", moon.vx, moon.vy, moon.vz, ")")
-        
-moons = [Moon((d[0], d[1], d[2])) for d in data]
+def apply_gravity(moonlist):
+    moonlist = list(moonlist)
+    def update(moon):
+        def velchange(othermoon):
+            posA = moon[0]
+            posB = othermoon[0]
+            return (1 if a < b else -1 if a > b else 0
+                    for a, b in zip(posA, posB))
+        total_dv = reduce(add_tuples, map(velchange, moonlist))
+        return (moon[0], add_tuples(moon[1], total_dv))
+    return map(update, moonlist)
 
-def timestep(moonlist):
-    # gravity
-    matched = set()
-    for i, moon in enumerate(moonlist):
-        for j, othermoon in enumerate(moonlist):
-            if i == j:
-                continue
-            if ((i, j) in matched) or ((j, i) in matched):
-                continue
-            if moon.x > othermoon.x:   
-                moon.vx -= 1
-                othermoon.vx += 1
-            elif moon.x < othermoon.x:
-                moon.vx += 1
-                othermoon.vx -= 1
-            if moon.y > othermoon.y:
-                moon.vy -= 1
-                othermoon.vy += 1
-            elif moon.y < othermoon.y:
-                moon.vy += 1
-                othermoon.vy -= 1
-            if moon.z > othermoon.z:
-                moon.vz -= 1
-                othermoon.vz += 1
-            elif moon.z < othermoon.z:
-                moon.vz += 1
-                othermoon.vz -= 1
-            matched.add((i, j))
-    # velocity
-    for moon in moonlist:
-        moon.x += moon.vx
-        moon.y += moon.vy
-        moon.z += moon.vz
+def apply_velocity(moonlist):
+    return ((add_tuples(moon[0], moon[1]), moon[1])
+            for moon in moonlist)
 
-def energy(moon):
-    pot = abs(moon.x) + abs(moon.y) + abs(moon.z)
-    kin = abs(moon.vx) + abs(moon.vy) + abs(moon.vz)
-    return pot * kin
+def get_energy(moonlist):
+    return sum(
+            sum(map(abs, pos))*sum(map(abs, vel))
+            for pos, vel in moonlist
+            )
 
+moons = map(parse_moonstring, lines)
 for i in range(1000):
-    timestep(moons)
-
-print(sum(map(energy, moons)))
-
-
-moons = [Moon((d[0], d[1], d[2])) for d in data]
-
-def staterepr(moonlist):
-    moontups = []
-    for moon in moonlist:
-        moontups.append((moon.x, moon.y, moon.z,
-                         moon.vx, moon.vy, moon.vz))
-    return tuple(moontups)
-
-ini = staterepr(moons)
-
-timestep(moons)
-i = 1
-while True:
-    if i % 100000 == 0:
-        print(i)
-    if staterepr(moons) == ini:
-        break
-    timestep(moons)
-    i += 1
-print(i)
+    moons = apply_gravity(moons)
+    moons = apply_velocity(moons)
+print(get_energy(moons))
