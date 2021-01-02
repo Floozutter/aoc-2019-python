168db477806eb21e28e16fd96fb8b6b0fd46b0b6
Floozutter <floozutter@gmail.com>
2021-01-01 23:16:17 -0800

    Clean day05

--- day05/main.py
+++ day05/main.py
@@ -10,17 +10,17 @@ class Mde(Enum):
 	IMM = 1
 from itertools import chain, repeat, islice
 from operator import methodcaller
-from typing import List, Dict, Callable, Iterable, ClassVar
+from typing import Dict, Callable, List, Iterable, ClassVar
 class Ico:
 	liv: bool
 	ptr: int
-	mem: List[int]
+	mem: Dict[int, int]
 	cal: Callable[[], int]
 	log: List[int]
 	def __init__(self, prg: Iterable[int], cal: Callable[[], int]) -> None:
 		self.liv = True
 		self.ptr = 0
-		self.mem = list(prg)
+		self.mem = dict(enumerate(prg))
 		self.cal = cal
 		self.log = list()
 	def get(self, mde: Mde) -> int:
@@ -30,13 +30,11 @@ class Ico:
 		return idx
 	def ste(self) -> None:
 		val = self.mem[self.get(Mde.IMM)]
-		opc = val % 100
 		mds = map(Mde, chain(map(int, reversed(str(val // 100))), repeat(0)))
-		opr = self.ops[opc]
+		opr = self.ops[val % 100]
 		opr(self, *map(self.get, islice(mds, opr.__code__.co_argcount - 1)))
 	def run(self) -> List[int]:
-		while self.liv:
-			self.ste()
+		while self.liv: self.ste()
 		return self.log
 	def add(self, idx: int, jdx: int, kdx: int) -> None:
 		self.mem[kdx] = self.mem[idx] + self.mem[jdx]
