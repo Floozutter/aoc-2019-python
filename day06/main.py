73ebb4ac8e36ef0b668175e4c8b209a675f4b871
Floozutter <floozutter@gmail.com>
2021-01-02 22:11:52 -0800

    Add day06 part 2

--- day06/main.py
+++ day06/main.py
@@ -3,11 +3,30 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
 	raw = ifile.read()
 from collections import defaultdict
-orbits = defaultdict(set)
+satellites = defaultdict(set)
+primaries = dict()
 for orbit in raw.split():
 	primary, satellite = orbit.split(")")
-	orbits[primary].add(satellite)
+	satellites[primary].add(satellite)
+	primaries[satellite] = primary
 
 def orbit_count(primary: str, depth: int = 0) -> int:
-	return depth + sum(orbit_count(s, depth + 1) for s in orbits[primary])
+	return depth + sum(orbit_count(s, depth + 1) for s in satellites[primary])
 print(orbit_count("COM"))
+
+from typing import Tuple
+def ancestors(satellite: str) -> Tuple[str]:
+	if (primary := primaries.get(satellite)) is not None:
+		return (primary,) + ancestors(primary)
+	else:
+		return ()
+from itertools import takewhile
+def orbital_transfers(a: str, b: str) -> str:
+	ancestors_a = ancestors(a)
+	ancestors_b = ancestors(b)
+	common = sum(1 for _ in takewhile(
+		lambda pair: pair[0] == pair[1],
+		zip(reversed(ancestors_a), reversed(ancestors_b))
+	))
+	return len(ancestors_a) + len(ancestors_b) - 2 * common
+print(orbital_transfers("YOU", "SAN"))
