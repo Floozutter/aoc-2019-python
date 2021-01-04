26962045eea0e8eb0782a55d572309abe07cf534
Floozutter <floozutter@gmail.com>
2021-01-04 07:24:49 -0800

    Add day10 part 2

--- day10/main.py
+++ day10/main.py
@@ -11,9 +11,36 @@ asteroids = frozenset(
 
 from math import gcd
 from typing import Tuple
-def angle(di: int, dj: int) -> Tuple[int, int]:
+def tuple_angle(di: int, dj: int) -> Tuple[int, int]:
 	divisor = max(gcd(di, dj), 1)
 	return di // divisor, dj // divisor
 def detectable_asteroids(i: int, j: int) -> int:
-	return len({angle(p - i, q - j) for p, q in asteroids}) - 1
-print(max(detectable_asteroids(i, j) for i, j in asteroids))
+	return len({tuple_angle(p - i, q - j) for p, q in asteroids}) - 1
+(station_i, station_j), station_n = max(
+	(((i, j), detectable_asteroids(i, j)) for i, j in asteroids),
+	key = lambda pair: pair[1]
+)
+print(station_n)
+
+from collections import defaultdict
+stacks = defaultdict(list)
+for i, j in asteroids:
+	di, dj = i - station_i, j - station_j
+	stacks[tuple_angle(di, dj)].append((di, dj))
+del stacks[0, 0]
+for stack in stacks.values():
+	stack.sort(reverse = True)
+from math import atan2, pi
+def order_key(angle: Tuple[int, int]) -> float:
+	i, j = angle
+	theta = atan2(-i, j)
+	return theta if theta > pi / 2 else theta + 2 * pi
+order = sorted(stacks, key = order_key, reverse = True)
+vaporized = []
+while any(stacks.values()):
+	for angle in order:
+		if stack := stacks[angle]:
+			vaporized.append(stack.pop())
+di, dj = vaporized[199]
+i, j = di + station_i, dj + station_j
+print(j * 100 + i)
