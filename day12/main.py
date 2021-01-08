25bf207e829feec5185caed081ec322f62adb96d
Floozutter <floozutter@gmail.com>
2021-01-07 21:58:46 -0800

    Add day12 part 2

--- day12/main.py
+++ day12/main.py
@@ -6,52 +6,59 @@ from typing import Tuple
 def line_to_pos(line: str) -> Tuple[int, int, int]:
 	filtered = "".join(c for c in line if c.isdigit() or c in {"-", ","})
 	return tuple(map(int, filtered.split(",")))
-start_positions = tuple(map(line_to_pos, raw.strip().split("\n")))
+starts = tuple(zip(*map(line_to_pos, raw.strip().split("\n"))))
 
 from itertools import combinations
 from typing import List, Iterable
-class System:
-	poss: List[List[int]]
-	vels: List[List[int]]
-	def __init__(self, poss: Iterable[Iterable[int]]) -> None:
-		self.poss = [list(pos) for pos in poss]
-		self.vels = [[0] * len(pos) for pos in self.poss]
+class Axis:
+	poss: List[int]
+	vels: List[int]
+	def __init__(self, poss: Iterable[int]) -> None:
+		self.poss = list(poss)
+		self.vels = [0] * len(self.poss)
 	def __eq__(self, other) -> bool:
 		return self.poss == other.poss and self.vels == other.vels
 	def step(self, n: int) -> None:
 		for _ in range(n):
 			for i, j in combinations(range(len(self.poss)), 2):
-				for ax, (a, b) in enumerate(zip(self.poss[i], self.poss[j])):
-					if a < b:
-						self.vels[i][ax] += 1
-						self.vels[j][ax] -= 1
-					elif a > b:
-						self.vels[i][ax] -= 1
-						self.vels[j][ax] += 1
-			for pos, vel in zip(self.poss, self.vels):
-				for i in range(len(pos)):
-					pos[i] += vel[i]
-	def total_energy(self) -> int:
-		return sum(
-			sum(map(abs, pos)) * sum(map(abs, vel))
-			for pos, vel in zip(self.poss, self.vels)
-		)
+				a, b = self.poss[i], self.poss[j]
+				diff = 1 if a < b else -1 if a > b else 0
+				self.vels[i] += diff
+				self.vels[j] -= diff
+			for i, vel in enumerate(self.vels):
+				self.poss[i] += vel
 
-system = System(start_positions)
-system.step(1000)
-print(system.total_energy())
+system = tuple(map(Axis, starts))
+for axis in system:
+	axis.step(1000)
+moon_poss = zip(*(axis.poss for axis in system))
+moon_vels = zip(*(axis.vels for axis in system))
+print(sum(
+	sum(map(abs, pos)) * sum(map(abs, vel))
+	for pos, vel in zip(moon_poss, moon_vels)
+))
 
-tort = System(start_positions)
-tort.step(1)
-hare = System(start_positions)
-hare.step(2)
-steps = 1
-from time import time
-t = time()
-while tort != hare:
-	if steps % 100_000 == 0: print(f"Steps: {steps:,}")
+def detect_cycle(poss: Iterable[int]) -> Tuple[int, int]:
+	tort = Axis(poss)
+	hare = Axis(tort.poss)
 	tort.step(1)
 	hare.step(2)
-	steps += 1
-print(f"Time Elapsed: {time() - t}")
-print(steps)
+	first_repeat = 1
+	while tort != hare:
+		tort.step(1)
+		hare.step(2)
+		first_repeat += 1
+	hare.step(1)
+	period = 1
+	while hare != tort:
+		hare.step(1)
+		period += 1
+	return first_repeat - period, period
+system_mu = 0
+system_period = 1
+from math import lcm
+for mu, period in map(detect_cycle, starts):
+	while system_mu % period != mu:
+		system_mu += system_period
+	system_period = lcm(system_period, period)
+print(system_mu + system_period)
