4be97ff49351beb663aa4c36389f92162dae789c
Floozutter <floozutter@gmail.com>
2021-01-07 22:25:38 -0800

    Clean day12

--- day12/main.py
+++ day12/main.py
@@ -3,7 +3,7 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
 	raw = ifile.read()
 from typing import Tuple
-def line_to_pos(line: str) -> Tuple[int, int, int]:
+def line_to_pos(line: str) -> Tuple[int, ...]:
 	filtered = "".join(c for c in line if c.isdigit() or c in {"-", ","})
 	return tuple(map(int, filtered.split(",")))
 starts = tuple(zip(*map(line_to_pos, raw.strip().split("\n"))))
@@ -13,52 +13,39 @@ from typing import List, Iterable
 class Axis:
 	poss: List[int]
 	vels: List[int]
-	def __init__(self, poss: Iterable[int]) -> None:
-		self.poss = list(poss)
+	def __init__(self, start_poss: Iterable[int]) -> None:
+		self.poss = list(start_poss)
 		self.vels = [0] * len(self.poss)
 	def __eq__(self, other) -> bool:
 		return self.poss == other.poss and self.vels == other.vels
-	def step(self, n: int) -> None:
-		for _ in range(n):
-			for i, j in combinations(range(len(self.poss)), 2):
-				a, b = self.poss[i], self.poss[j]
-				diff = 1 if a < b else -1 if a > b else 0
-				self.vels[i] += diff
-				self.vels[j] -= diff
-			for i, vel in enumerate(self.vels):
-				self.poss[i] += vel
+	def step(self) -> None:
+		for i, j in combinations(range(len(self.poss)), 2):
+			a, b = self.poss[i], self.poss[j]
+			diff = 1 if a < b else -1 if a > b else 0
+			self.vels[i] += diff
+			self.vels[j] -= diff
+		for i, vel in enumerate(self.vels):
+			self.poss[i] += vel
 
 system = tuple(map(Axis, starts))
 for axis in system:
-	axis.step(1000)
-moon_poss = zip(*(axis.poss for axis in system))
-moon_vels = zip(*(axis.vels for axis in system))
+	for _ in range(1000):
+		axis.step()
+pos_by_moon = zip(*(axis.poss for axis in system))
+vel_by_moon = zip(*(axis.vels for axis in system))
 print(sum(
 	sum(map(abs, pos)) * sum(map(abs, vel))
-	for pos, vel in zip(moon_poss, moon_vels)
+	for pos, vel in zip(pos_by_moon, vel_by_moon)
 ))
 
-def detect_cycle(poss: Iterable[int]) -> Tuple[int, int]:
-	tort = Axis(poss)
-	hare = Axis(tort.poss)
-	tort.step(1)
-	hare.step(2)
-	first_repeat = 1
-	while tort != hare:
-		tort.step(1)
-		hare.step(2)
-		first_repeat += 1
-	hare.step(1)
-	period = 1
+def cycle_period(start_poss: Iterable[int]) -> int:
+	tort = Axis(start_poss)  # Get some rest, buddy. :3
+	hare = Axis(tort.poss)  # Up for a run? >:3c
+	hare.step()
+	steps = 1
 	while hare != tort:
-		hare.step(1)
-		period += 1
-	return first_repeat - period, period
-system_mu = 0
-system_period = 1
+		hare.step()
+		steps += 1
+	return steps
 from math import lcm
-for mu, period in map(detect_cycle, starts):
-	while system_mu % period != mu:
-		system_mu += system_period
-	system_period = lcm(system_period, period)
-print(system_mu + system_period)
+print(lcm(*map(cycle_period, starts)))
